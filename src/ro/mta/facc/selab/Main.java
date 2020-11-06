eb672df1d21ef7734578d07972d13e8da803f4ee
dan-avram-selab <dan.avram@mta.ro>
2020-11-06 12:28:09 +0200

    Adaugare carte
    Signed-off-by: dan-avram-selab <dan.avram@mta.ro>

--- src/ro/mta/facc/selab/Main.java
+++ src/ro/mta/facc/selab/Main.java
@@ -3,12 +3,14 @@ package ro.mta.facc.selab;
 public class Main {
     public static void main(String[] args) {
         Carte carte = new Carte("Fratii Karamazov", "Feodor Dostoievski", 1880, 1200);
+        Carte carte2 = new Carte("Fratii Karamazov", "Feodor Dostoievski", 1880, 1200);
         Biblioteca biblioteca = new Biblioteca();
 
         biblioteca.add(carte);
         biblioteca.add(new Carte("Procesul", "F. Kafka", 1910, 350));
         biblioteca.add(new Carte("Ender's Game", "Orscon Scott Card", 1954, 330));
 
+		biblioteca.add(carte2);
         biblioteca.sort();
         // System.out.println(biblioteca);
 
